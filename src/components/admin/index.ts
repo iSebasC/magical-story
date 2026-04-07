240d7318a61e2527a32a2ef49c79bffa97042713
cabreralcalasebastian <cabreralcalasebastian@gmail.com>
2026-04-07 01:56:18 -0500

    admin 2

--- src/components/admin/index.ts
+++ src/components/admin/index.ts
@@ -4,8 +4,3 @@ export { AdminTopbar } from './AdminTopbar';
 export { StatsCard } from './StatsCard';
 export { TopStories } from './TopStories';
 export { RecentActivity } from './RecentActivity';
-export { StoriesUploadZone } from './StoriesUploadZone';
-export { UploadProgress } from './UploadProgress';
-export { StoryForm } from './StoryForm';
-export { StoriesTable } from './StoriesTable';
-export { UsersTable } from './UsersTable';
